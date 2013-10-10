4265e096744783e2db0fa6658fb14388d03d51d8
信雅达 <sunyard@shinmiyabidamatoMacBook-Air.local>
2013-10-10 16:27:46 +0800

    Initial Commit

---
+++ SYDMapView/SYDMapView.h
@@ -0,0 +1,13 @@
+//
+//  SYDMapView.h
+//  SYDMapView
+//
+//  Created by 信雅达 on 13-10-10.
+//  Copyright (c) 2013年 施智龙. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SYDMapView : NSObject
+
+@end
---
+++ SYDMapView/SYDMapView.m
@@ -0,0 +1,13 @@
+//
+//  SYDMapView.m
+//  SYDMapView
+//
+//  Created by 信雅达 on 13-10-10.
+//  Copyright (c) 2013年 施智龙. All rights reserved.
+//
+
+#import "SYDMapView.h"
+
+@implementation SYDMapView
+
+@end
